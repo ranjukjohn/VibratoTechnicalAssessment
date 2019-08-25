06e9a1b87621048cc77e9a25fb316dff82f72b6b
Ranju John <ranjukj@yahoo.com>
2019-08-25 11:59:04 +1000

    - Adding hashbang - Creating default nodejs app Vibrato Tech Assess (vtc) app - vtcapp - launch the node server. Use http://<ipadd>:3000 to view the app

--- webapp/setup.sh
+++ webapp/setup.sh
@@ -1,17 +1,32 @@
+#!/usr/bin/env bash
+# Update all packages
 sudo yum update -y
-
-mkdir webapp
-cd webapp
 
 # Install node version manager (nvm)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-#Close and reopen your terminal to start using nvm or run the following to use it now:
+
+# Close and reopen your terminal to start using nvm or run the following to use it now:
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # Activate nvm
 . ~/.nvm/nvm.sh
+
 # Use nvm to install the latest version of Node.js
 nvm install node
+
 # Test that Node.js is installed and running correctly
 node -e "console.log('Running Node.js ' + process.version)"
+
+# Use the express-generator that creates an application skeleton
+npm install express-generator -g
+
+# Create a the Vibrato Tech Assessment (vtc) app  with express-generator
+express -v ejs -c sass vtcapp
+
+cd vtcapp
+npm install
+
+# Start the node server
+npm start &
